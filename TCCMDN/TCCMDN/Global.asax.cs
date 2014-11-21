2e33313a8f6c7879c65457c036fcab957c413129
Michael Duvoisin <michaelduvoisin@hotmail.com>
2014-11-21 11:54:42 +0100

    no message

--- TCCMDN/TCCMDN/Global.asax.cs
+++ TCCMDN/TCCMDN/Global.asax.cs
@@ -7,16 +7,25 @@ using System.Web.Routing;
 using System.Web.Security;
 using TCCMDN;
 
+
+
+
 namespace TCCMDN
 {
     public class Global : HttpApplication
     {
+        public static string version;
+
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterOpenAuth();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // version
+            version = " 0.1";
+
         }
 
         void Application_End(object sender, EventArgs e)
@@ -30,5 +39,7 @@ namespace TCCMDN
             // Code that runs when an unhandled error occurs
 
         }
+
+
     }
 }
